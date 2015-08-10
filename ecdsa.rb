1c9b53f980a4ab9f9da9b6a968e5e2ea14126002
Stephen Blackstone <sblackstone@gmail.com>
2015-08-10 13:32:36 -0400

    Fix bug in generate_signature, now verifies correctly

--- ecdsa.rb
+++ ecdsa.rb
@@ -9,27 +9,23 @@ class ECDSA
 
   def verify_signature(message, r,s, qa)
     e = Digest::SHA256.hexdigest(message)
-    z = e.to_i(16)
+    z = e.to_i(16) % @n
     w = MathHelpers.mult_inv(s, @n)
     u1 = (z*w) % @n
     u2 = (r*w) % @n
-    p = (@g*u1) + (qa*u2)
-    
-    pp p.x % @n
-    pp r % @n
-    
-    return p.x % @n == r % @n
-    
+    p = (@g*u1) + (qa*u2)    
+    return (p.x % @n) == (r % @n)    
   end
   
 
   def generate_signature(private_key, message)
     e = Digest::SHA256.hexdigest(message)
-    z = e.to_i(16)
+    z = e.to_i(16) % @n
     k = 0
     s = 0
     while s == 0
       r = 0
+      s = 0 
       while r == 0
         k = generate_random_k
         point = @g * k
@@ -38,7 +34,7 @@ class ECDSA
       k_inv = MathHelpers.mult_inv(k,@n)
       s = (k_inv * (z + r*private_key)) % @n
     end
-    return {:r => r, :s => s, :public_key => point}
+    return {:r => r, :s => s, :public_key => @g * private_key }
   end
   
 
@@ -58,5 +54,4 @@ class ECDSA
     prng.rand(@n-1)
   end
   
-
 end
--- test.rb
+++ test.rb
@@ -1,15 +1,13 @@
 require './ecdsa.rb'
 
-
-
-message = "We Launch At Midnight"
 ecdsa = ECDSA.new
 
-sig = ecdsa.generate_signature(12345, message)
+message = "We Launch At Midnight"
 
-print "Signature = "
-pp sig
+sig = ecdsa.generate_signature(0x08f96d2fa8a7d46598acb969aa09ed54caf079235ed0203763640cc1b8917055, message)
 
+puts sig[:public_key].x.to_s(16) 
+puts sig[:public_key].y.to_s(16) 
 
 pp ecdsa.verify_signature(message, sig[:r], sig[:s], sig[:public_key])
 
