60c6749c954c9efde80433be9492757dab0cf62e
Stephen Blackstone <sblackstone@gmail.com>
2015-08-10 11:38:41 -0400

    Wrote initial verify code, its not quite right yet

---
+++ ecdsa.rb
@@ -0,0 +1,62 @@
+require 'pp'
+require 'digest'
+require './math_helpers.rb'
+require './point.rb'
+require './curve.rb'
+
+
+class ECDSA
+
+  def verify_signature(message, r,s, qa)
+    e = Digest::SHA256.hexdigest(message)
+    z = e.to_i(16)
+    w = MathHelpers.mult_inv(s, @n)
+    u1 = (z*w) % @n
+    u2 = (r*w) % @n
+    p = (@g*u1) + (qa*u2)
+    
+    pp p.x % @n
+    pp r % @n
+    
+    return p.x % @n == r % @n
+    
+  end
+  
+
+  def generate_signature(private_key, message)
+    e = Digest::SHA256.hexdigest(message)
+    z = e.to_i(16)
+    k = 0
+    s = 0
+    while s == 0
+      r = 0
+      while r == 0
+        k = generate_random_k
+        point = @g * k
+        r = point.x % @n
+      end
+      k_inv = MathHelpers.mult_inv(k,@n)
+      s = (k_inv * (z + r*private_key)) % @n
+    end
+    return {:r => r, :s => s, :public_key => point}
+  end
+  
+
+  def initialize
+    # Secp256k1 Params
+    @p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+    @a = 0
+    @b = 7
+    @e = Curve.new(@a,@b,@p)
+    @g = @e.point(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
+    @n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141    
+  end
+
+  # THIS IS NOT SECURE!
+  def generate_random_k
+    prng = Random.new
+    prng.rand(@n-1)
+  end
+  
+
+end
--- test.rb
+++ test.rb
@@ -1,49 +1,15 @@
-require 'digest'
-require 'pp'
-require './math_helpers.rb'
-require './point.rb'
-require './curve.rb'
-
-
-class ECDSA
-
-  def generate_signature(message)
-    e = Digest::SHA256.hexdigest(message)
-    da = generate_private_key
-    qa = @g * da
-    pp qa
-    pp e
-    r = 0
-    while r == 0
-      k = generate_private_key
-      point = @g * k
-      r = point.x % @n
-    end
-    pp r
-  end
-  
-
-  # Secp256k1 Params
-  def initialize
-    @p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
-    @a = 0
-    @b = 7
-    @e = Curve.new(@a,@b,@p)
-    @g = @e.point(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
-    @n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141    
-  end
-
-  # THIS IS NOT SECURE!
-  def generate_private_key
-    prng = Random.new
-    prng.rand(@n)
-  end
-  
-
-end
+require './ecdsa.rb'
 
 
 
-
+message = "We Launch At Midnight"
 ecdsa = ECDSA.new
-ecdsa.generate_signature("We Launch at midnight")
+
+sig = ecdsa.generate_signature(12345, message)
+
+print "Signature = "
+pp sig
+
+
+pp ecdsa.verify_signature(message, sig[:r], sig[:s], sig[:public_key])
+
